cfa06294237b82fd5f4b4f74a9a07fc50c4d65b9
Ivan Ledesma <ivanarielledesma@gmail.com>
2025-04-07 13:30:53 -0300

    Ejercicio de geometria completado.

--- ejemplos/03.geometria/geometria.py
+++ ejemplos/03.geometria/geometria.py
@@ -17,6 +17,31 @@ class Rectangulo(Figura):
         return 2*self.lado1 + 2*self.lado2
 
 # TODO: Pensar en otra posible herencia (taxonomía)
+# Otra forma posible seria
+
+class Figura:
+    pass
+
+class Poligono(Figura):
+    def __init__(self, lados):
+        self.lados = lados
+
+    def perimetro(self):
+        return sum(self.lados)
+
+class Cuadrado(Poligono):
+    def __init__(self, lado):
+        super().__init__([lado]*4)
+
+class Rectangulo(Poligono):
+    def __init__(self, lado1, lado2):
+        super().__init__([lado1, lado2, lado1, lado2])
+
+cuad = Cuadrado(5)
+print("Perímetro del cuadrado:", cuad.perimetro())  # 20
+
+rect = Rectangulo(4, 6)
+print("Perímetro del rectángulo:", rect.perimetro())  # 20
 
 # Pruebas
 c1 = Cuadrado(6)
