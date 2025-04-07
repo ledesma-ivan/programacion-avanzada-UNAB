361ef10009ceef10b52852e300939307033a58bb
Ivan Ledesma <ivanarielledesma@gmail.com>
2025-04-07 12:56:12 -0300

    Completando el ejercicio de ejemplo de animal.

--- ejemplos/02.animales/animal.py
+++ ejemplos/02.animales/animal.py
@@ -1,30 +1,35 @@
 class Animal:
 
     # Edad es un argumento opcional
-    def __init__(self, nombre, edad = -1):
+    def __init__(self, nombre, edad=-1):
         self.nombre = nombre
         self.edad = edad
 
     def hablar(self):
         print('Soy ' + self.nombre + ' y digo .....')
 
+
 class Perro(Animal):
     texto_hablar = 'Guau!'
 
     def hablar(self):
-        print('Soy ' + self.nombre + ' y digo ' + self.texto_hablar)   
+        print('Soy ' + self.nombre + ' y digo ' + self.texto_hablar)
+
 
 class Gato(Animal):
     def hablar(self):
         print('Soy ' + self.nombre + ' y digo Miau!')
 
+
 class Raton(Animal):
-    pass
+    def hablar(self):
+        print('Soy ' + self.nombre + ' y tengo de edad', self.edad)
+
 
 class Gatito(Gato):
-    pass
+    def hablar(self):
+        print('Soy ' + self.nombre + ' y digo Miau!')
 
-for elem in Perro('Spike'), Gato('Tom'), Raton('Jerry', 3), Gatito("Mininito"): 
+
+for elem in Perro('Spike'), Gato('Tom'), Raton('Jerry', 3), Gatito("Mininito"):
     elem.hablar()
-   
-
