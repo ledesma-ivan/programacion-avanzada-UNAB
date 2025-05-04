c9ab0177140ec684a12bc78f72757dea53609b6b
Ivan Ledesma <ivanarielledesma@gmail.com>
2025-05-04 12:04:03 -0300

    Avanti, probando funciones

--- practica/ejercicios-programacion-estructurada/ejercicios_pre_parcial.py
+++ practica/ejercicios-programacion-estructurada/ejercicios_pre_parcial.py
@@ -40,3 +40,35 @@ for i in lista:
         x = i * 3
         lista_nueva.append(x)
 print(lista_nueva)
+
+
+'''
+¿Qué diferencias notaste entre escribir código imperativo y usar comprensiones o funciones
+como map o filter?
+
+En que en la las compresiones pueden ser mas confunsa si no entendes mucho de listas, pero es mas funcional
+
+� Desafío opcional
+Convertí el mismo ejercicio imperativo a uno declarativo (usando list comprehension) y
+compará claridad y legibilidad.
+'''
+nums = [1, 2, 3, 4]
+lista_new = []
+
+# Usando map y filter repasar un poco aca.
+
+# filter: se usa para filtrar elementos segun condicion
+# map se usa una funcion a cada elemento de la lista
+
+# lambda argumentos: expresión , condicion, elemento
+lista_filtrada = filter(lambda x: x % 2 == 0, lista)
+
+# Map
+lista_nueva_map = list(map(lambda x: x * 2, lista_filtrada))
+print(lista_nueva_map)
+
+
+# [expresión for elemento in iterable if condición]
+
+pares = [i * 2 for i in nums if i % 2 == 0]
+print(pares)
